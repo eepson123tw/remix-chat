345752c234b969af9694183b5c904159b1157835
allen <eepson123@gmail.com>
2024-06-08 18:29:04 +0800

    fix(package.json): fix the serve error

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,8 +1,4 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002,
+  ignoredRouteFiles: ["**/.*"],
 };
